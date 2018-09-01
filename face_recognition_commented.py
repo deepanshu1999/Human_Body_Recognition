519165e35393c90d78f33b046ad2734e880d5f73
Deepanshu Aggarwal <dadeepagg2@gmail.com>
2018-09-01 08:52:18 +0530

    Added Basic Code

---
+++ face_recognition_commented.py
@@ -0,0 +1,26 @@
+# Face Recognition
+
+import cv2
+import matplotlib.pyplot as plt
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # We load the cascade for the face.
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') # We load the cascade for the eyes.
+
+def detect(gray, frame): # We create a function that takes as input the image in black and white (gray) and the original image (frame), and that will return the same image with the detector rectangles. 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) # We apply the detectMultiScale method from the face cascade to locate one or several faces in the image.
+    for (x, y, w, h) in faces: # For each detected face:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) # We paint a rectangle around the face.
+        roi_gray = gray[y:y+h, x:x+w] # We get the region of interest in the black and white image.
+        roi_color = frame[y:y+h, x:x+w] # We get the region of interest in the colored image.
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3) # We apply the detectMultiScale method to locate one or several eyes in the image.
+        for (ex, ey, ew, eh) in eyes: # For each detected eye:
+            cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
+    return frame # We return the image with the detector rectangles.
+
+from PIL import Image
+img = Image.open('Test.jpg').convert("L")
+img.save('greyscale.jpg')
+frame=cv2.imread('greyscale.jpg')
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # We do some colour transformations.
+canvas = detect(gray, frame) # We get the output of our detect function.
+plt.imshow(canvas)
